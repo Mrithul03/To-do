47007418f8c4b57b58ee91248931eefc4d7a2030
Mrithul03 <mrithul.mridulayam@gmail.com>
2025-02-04 18:55:40 +0530

    'second'

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css'
 import To_do from './components/To_do';
 
--- src/components/To_do.js
+++ src/components/To_do.js
@@ -1,15 +1,31 @@
 import React, { useState } from 'react'
 import './To_do.css'
+import * as Yup from 'yup';
 
 function To_do() {
+
     const [taskname, settaskname] = useState('')
     const [tasks, settasks] = useState([])
     const [status, setstatus] = useState('incomplete')
-    const handlesubmit = (e) => {
+
+    const validationSchema = Yup.object().shape({
+        taskname: Yup.string().min(10, "Must be at least 10 characters").required('Task name is required'),
+        status: Yup.string().required("Status is required"),
+    })
+    const handlesubmit = async (e) => {
         e.preventDefault()
-        if (taskname != '' && status != '') {
+        let formdata = {
+            taskname: e.target.value,
+            status:e.target.value
+        }
+        const isvalid =await validationSchema.isValid(formdata)
+        
+       
+
+        if (taskname !='' && status != '') {
             let newtask = { id: Math.random(), title: taskname, status: status }
             settasks([...tasks, newtask])
+            
             settaskname("")
         }
     }
@@ -32,13 +48,42 @@ function To_do() {
         }
         settasks(tem)
     }
+
+    const handleEdit = (id) => {
+        let tem = [...tasks]
+        for (var i = 0; i < tem.length; i++) {
+            if (tem[i].id == id) {
+                settaskname(tem[i].title)
+                setstatus(tem[i].status)
+                setid(tem[i].id)
+            }
+        }
+    }
+    const [id, setid] = useState(false)
+
+    const handleUpdate = (id) => {
+        let tem = [...tasks]
+        for (var i = 0; i < tem.length; i++) {
+            if (tem[i].id == id) {
+                tem[i].status = status
+                tem[i].title = taskname
+                setid('')
+                settaskname("")
+            }
+        }
+        settasks(tem)
+    }
+
+
     return (
         <div className='main'>
-            <h1 class="text-3xl font-bold underline">TO-DO-LIST</h1>
-            <form onSubmit={handlesubmit}>
+            <h1>TO-DO-LIST</h1>
+            <form onSubmit={handlesubmit} >
                 <div>
                     <label>
-                        Task:<input type='text' value={taskname} onChange={(e) => settaskname(e.target.value)} />
+                        Task:<input type='text' value={taskname} onChange={(e) => settaskname(e.target.value, id)} />
+                        
+                        {error}
                     </label>
                 </div>
                 <div>
@@ -49,11 +94,12 @@ function To_do() {
                     </select>
                 </div>
                 <div>
-                    <button type='submit' value='submit'>Submit</button>
+                    {id ? <button onClick={() => handleUpdate(id)}>UPDATE</button> : <button type='submit' value='submit'>Submit</button>}
+
                 </div>
             </form>
 
-            <table class='table'>
+            <table className='table'>
                 <tbody>
                     <tr>
                         <td>ID</td>
@@ -61,6 +107,7 @@ function To_do() {
                         <td>STATUS</td>
                     </tr>
                     {tasks.map((t) => (
+
                         <tr>
                             <td>{t.id}</td>
                             <td>{t.title}</td>
@@ -69,6 +116,7 @@ function To_do() {
                                 <option value='incomplete'>Incomplete</option>
                             </select></td>
                             <td><button onClick={() => handledelete(t.id)} >DELETE</button></td>
+                            <td><button onClick={() => handleEdit(t.id)} >EDIT</button></td>
                         </tr>
                     ))
                     }
