1ba348001d44d435abaf7fdd962c5cbfb993a4a8
Mrithul03 <mrithul.mridulayam@gmail.com>
2025-02-05 18:48:12 +0530

    css

--- src/components/To_do.js
+++ src/components/To_do.js
@@ -3,6 +3,7 @@ import './To_do.css'
 import * as Yup from 'yup';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import {}  from 'formik'
 
 function To_do() {
 
@@ -18,32 +19,37 @@ function To_do() {
             .oneOf(["Completed", "incomplete"], "Select the status")
             .required("Status is required"),
     })
-    const [message, setmessage] = useState()
+    const [message, setmessage] = useState({})
     const handlesubmit = async (e) => {
         e.preventDefault()
         try {
-            // const validity = await validationSchema.isValid({ taskname, status })
-            // console.log(validity)
+            if(await validationSchema.isValid({ taskname, status })){
+                if (taskname != '' && status != '') {
+                    let newtask = { id: Math.random(), title: taskname, status: status }
+                    settasks([...tasks, newtask])
+                    toast("ADDED");
+                    settaskname("")
+                    setmessage('')
+                }
+            }else{
+                await validationSchema.validate({ taskname, status }, { abortEarly: false })
+                console.log(validationSchema)
 
-            await validationSchema.validate({ taskname, status }, { abortEarly: false })
-            console.log(validationSchema)
-
-            if (taskname != '' && status != '') {
-                let newtask = { id: Math.random(), title: taskname, status: status }
-                settasks([...tasks, newtask])
-                toast("ADDED");
-                settaskname("")
-            }
+            }    
         } catch (err) {
             // err.errors.map((error) => {
             //     toast.error(error)
             // });
-            setmessage(err.errors.map((error) => (`${error},`)))
-            console.log(err.errors)
-
+            // setmessage(err.errors.map((error) => (`${error},`)))
+            console.log(err.errors);  
+            const newerr={}
+            err.inner.map((error)=>{
+              newerr[error.path]=error.message
+            })
+            setmessage(newerr)
+            console.log(message)
             
-            
-        }
+        }   
     }
     const handledelete = (id) => {
         let tem = [...tasks]
@@ -95,29 +101,33 @@ function To_do() {
 
 
     return (
-        <div className='main'>
+        <div className='to-do-container'>
             <h1>TO-DO-LIST</h1>
             <ToastContainer />
             <form onSubmit={handlesubmit} >
                 <div>
                     <label>
-                        Task:<input type='text' value={taskname} onChange={(e) => settaskname(e.target.value, id)} />
+                        Task:<input 
+                        className='input_btn'
+                        type='text' 
+                        placeholder='Enter your task'
+                        value={taskname} 
+                        onChange={(e) => settaskname(e.target.value, id)} />
+                       {message.taskname && <div id="error">{message.taskname}</div>}
                     </label>
-                </div>
-                <div>
                     <label>Status:</label>
-                    <select value={(status)} onChange={(e) => setstatus(e.target.value)}>
+                    <select className='input_btn' value={(status)} onChange={(e) => setstatus(e.target.value)}>
                         <option value='Completed'>Completed</option>
                         <option value='incomplete'>Incomplete</option>
                     </select>
+                    {message.status && <span id='error'> {message.status}</span>}
+                    {id ? <button id='btn' onClick={() => handleUpdate(id)}>UPDATE</button> : <button id='btn' type='submit' value='submit'>Submit</button>}
                 </div>
-                <div>
-                    <span id='error'>{message}</span>
-                </div>
-                <div>
-                    {id ? <button onClick={() => handleUpdate(id)}>UPDATE</button> : <button type='submit' value='submit'>Submit</button>}
+                
+                
+                    
 
-                </div>
+                
             </form>
 
             <table className='table'>
@@ -136,8 +146,8 @@ function To_do() {
                                 <option value='Completed'>Completed</option>
                                 <option value='incomplete'>Incomplete</option>
                             </select></td>
-                            <td><button onClick={() => handledelete(t.id)} >DELETE</button></td>
-                            <td><button onClick={() => handleEdit(t.id)} >EDIT</button></td>
+                            <td><button className='edit-btn' onClick={() => handledelete(t.id)} >DELETE</button></td>
+                            <td><button className='delete-btn' onClick={() => handleEdit(t.id)} >EDIT</button></td>
                         </tr>
                     ))
                     }
